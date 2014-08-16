3ce7e21357ea4a3217ebf2c794f264a8453c0f8a
Drockio <derek.dunnom@gmail.com>
2014-08-15 18:46:45 -0700

    adding routing

--- CMS/App_Start/RouteConfig.cs
+++ CMS/App_Start/RouteConfig.cs
@@ -13,11 +13,38 @@ namespace CMS
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //    routes.MapRoute(
+            //        name: "Default",
+            //        url: "{controller}/{action}/{id}",
+            //        defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //    );
+
+            
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "Router",
+                url: "{tierOne}/{tierTwo}/{tierThree}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Router",
+                    tierOne = "Index",
+                    tierTwo = UrlParameter.Optional,
+                    tierThree = UrlParameter.Optional
+                }
             );
+
+            //routes.MapRoute(
+            //    name: "Router_with_subpages",
+            //    url: "{page}/{subpage}",
+            //    defaults: new { controller = "Home", action = "Router", id = UrlParameter.Optional }
+            //);
         }
     }
 }
--- CMS/Controllers/HomeController.cs
+++ CMS/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace CMS.Controllers
     {
         public ActionResult Index()
         {
+            //RouteConfig will send us to the declared action if 
+
             return View();
         }
 
@@ -25,6 +27,29 @@ namespace CMS.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        private ViewEngineResult ReturnView(string name)
+        {
+            return ViewEngines.Engines.FindView(ControllerContext, name, null);
+            
+        }
+
+        public ActionResult Router(string tierOne, string tierTwo = null, string tierThree = null)
+        {
+            var view = ReturnView(tierOne);
+            if (view.View != null)
+            {
+                //this is a view that is DECLARED.  It has an ActionResult in this controller.
+                //later we will put a case statement here that calls the appropriate ActionResult
+                return View(view);
+            }
+            else
+            {
+                //this is a page that is included in our CMS (page viewable via the DB)
+                //in this function we will query the DB and get the content for the page.  We will route it through a default view.
+                return View("index");
+            }
         }
     }
 }
